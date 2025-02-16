fa989c014ad14841bd8598bf8a743713fe06b54c
jmunoz <jmunoz@splunk.com>
2025-02-16 18:00:57 +0100

    manual inst

--- bin/entry.js
+++ bin/entry.js
@@ -1,6 +1,11 @@
-#!/usr/bin/env node
 const { start } = require('@splunk/otel');
 
 start({
    serviceName: 'pacman'
+}).then(() => {
+    console.log("✅ OpenTelemetry instrumentation started");
+    require('./server');  // Ensure server.js starts AFTER OpenTelemetry
+}).catch((err) => {
+    console.error("❌ OpenTelemetry failed to start:", err);
 });
+
