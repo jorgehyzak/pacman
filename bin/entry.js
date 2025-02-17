9cf2ad53a3fa41e9494e101fc719f5b15a56fe7e
jmunoz <jmunoz@splunk.com>
2025-02-17 14:39:50 +0100

    adding manual instruementation to get user details

--- bin/entry.js
+++ bin/entry.js
@@ -1,8 +1,16 @@
 const { start } = require('@splunk/otel');
+const opentelemetry = require('@opentelemetry/api');
 
 start({
-    serviceName: process.env.OTEL_SERVICE_NAME
+    serviceName: process.env.OTEL_SERVICE_NAME || "pacman-service"
 });
+
+// Export tracer for use in other files
+const tracer = opentelemetry.trace.getTracer("pacman-tracer");
+
+module.exports = tracer; // Export the tracer for use in other files
 
 // ✅ Start the application server
 require('./server');
+
+
